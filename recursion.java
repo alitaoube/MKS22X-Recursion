63b1b628671c916ad5860db186538323ea245a11
Ali Taoube <ataoube00@stuy.edu>
2019-02-02 10:07:56 -0500

    Added a driver to test, the loop is infinite due to the base case not working properly.

--- recursion.java
+++ recursion.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
 public class recursion{
+  public static void main(String[] args) {
+    System.out.println(sqrt(100, 1));
+  }
     /*You may write additional private methods */
 
     /*Recursively find the sqrt using Newton's approximation
@@ -9,26 +12,30 @@ public class recursion{
 
     */
     public static double sqrt(double n, double tolerance){
-      return sq(n, tolerance, 1);
+      return sq(n, tolerance, n / 2);
     }
 
     public static double sq(double n, double tolerance, double guess){
-      if (guess*guess / n <= tolerance){
+      System.out.println("n STATEMENT: " + n / guess*guess + "TOLERANCE: " + tolerance);
+      if (n / guess <= tolerance){
+        System.out.println("HERE");
         return guess;
       }
-      return sq(n, tolerance, ((n / guess + guess) / 2));
+      guess = (n / guess + guess) / 2;
+      return sq(n, tolerance, guess);
     }
 
-    /*Recursively find the n'th fibbonaci number in linear time
-     *fib(0) = 1; fib(1) = 1; fib(5) = 5
-     *precondition: n is non-negative
-     */
-    public static int fib(int n){
 
-    }
-
-    /*As Per classwork*/
-    public static ArrayList<Integer> makeAllSums(){
-    }
+    // /*Recursively find the n'th fibbonaci number in linear time
+    //  *fib(0) = 1; fib(1) = 1; fib(5) = 5
+    //  *precondition: n is non-negative
+    //  */
+    // public static int fib(int n){
+    //
+    // }
+    //
+    // /*As Per classwork*/
+    // public static ArrayList<Integer> makeAllSums(){
+    // }
 
 }
